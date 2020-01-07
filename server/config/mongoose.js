de1633101885a989b050bf1f260b3cc7d21b4ccb
Michelle Tanzil <mtanzil01@gmail.com>
2020-01-07 13:32:22 -0800

    added additional comments for config

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,8 +1,11 @@
 var fs = require("fs");
 var path = require("path");
-
-var models_path = path.join(__dirname, "./../models");
-
+// create a variable that points to the models folder
+var models_path = path.join(__dirname, './../models');
+// read all of the files in the models_path and require (run) each of the javascript files
 fs.readdirSync(models_path).forEach(function(file) {
-  if (file.indexOf(".js") >= 0) require(models_path + "/" + file);
-});
+  if(file.indexOf('.js') >= 0) {
+    // require the file (this runs the model file which registers the schema)
+    require(models_path + '/' + file);
+   }
+})
